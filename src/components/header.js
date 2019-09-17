20f724a6b89fcfac3c8cd7459d224da2cc8f7f66
Richard Garrison <azmobiledev@gmail.com>
2019-09-16 22:04:36 -0700

    Add sass, scss modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+//import './header.module.scss'
+import headerStyles from './header.module.scss'
+
 const Header = () => {
     return(
-        <header>
+        <header className={headerStyles.header}>
             <h1>Richard Garrison</h1>
             <nav>
                 <ul>
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link className={headerStyles.link} to="/">Home</Link>
                     </li>
                     <li>
                         <Link to="/blog">Blog</Link>
