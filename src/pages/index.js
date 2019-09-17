49e0bfc51d6ad49259b18021db226cd9fec51138
Richard Garrison <azmobiledev@gmail.com>
2019-09-16 17:45:49 -0700

    1st commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return(
+        <div>
+            <h1>GMR Plumbing</h1>
+            <h2>Residential and Commerical</h2>
+        </div>
+    )
+}
+
+
+
+
+export default IndexPage
