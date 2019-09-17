33dddc54a3c54d3b8496d6250993d39785e8b4a4
Richard Garrison <azmobiledev@gmail.com>
2019-09-16 19:34:07 -0700

    contact me link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutMe = () => {
     return(
         <div>
             <h1>About Me</h1>
             <p>I'm available for freelance work anytime!</p>
+            <p>Want to work with me? Reach out. <Link to="/contact"> Contact me</Link></p>
         </div>
     )
 }
