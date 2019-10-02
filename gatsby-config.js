3f909d5aba62a41432b7c3ae2dc8c32bb8c15195
Richard Garrison <azmobiledev@gmail.com>
2019-10-01 22:15:34 -0700

    GraphQL Playground, add mark down

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,19 @@
 
 module.exports = {
   /* Your site config here */
+      siteMetadata: {
+        title: 'Full-Stack Bootcamp',
+        author: 'R Smith'
+      },
   plugins: [
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-transformer-remark'
   ]
 }
